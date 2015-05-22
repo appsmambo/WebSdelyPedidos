ae2bfda25c9c1d551e2902d7dfa06ebcbd2a843b
appsmambo <quintanilla.peru@gmail.com>
2015-05-22 14:08:51 -0500

    ajustes

--- proyecto/SdelyPedidos/app/views/admin/usuario/listar.blade.php
+++ proyecto/SdelyPedidos/app/views/admin/usuario/listar.blade.php
@@ -11,42 +11,64 @@
 @stop
 @section('contenido')
 
-<table class="table table-striped table-bordered">
-	<thead>
-		<tr>
-			<th> Free Resource </th>
-			<th> Download</th>
-			<th class="td-actions"> </th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td> Fresh Web Development Resources </td>
-			<td> http://www.egrappler.com/ </td>
-			<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-		</tr>
-		<tr>
-			<td> Fresh Web Development Resources </td>
-			<td> http://www.egrappler.com/ </td>
-			<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-		</tr>
-		<tr>
-			<td> Fresh Web Development Resources </td>
-			<td> http://www.egrappler.com/ </td>
-			<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-		</tr>
-		<tr>
-			<td> Fresh Web Development Resources </td>
-			<td> http://www.egrappler.com/ </td>
-			<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-		</tr>
-		<tr>
-			<td> Fresh Web Development Resources </td>
-			<td> http://www.egrappler.com/ </td>
-			<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-		</tr>
+<div class="row">
+	<div class="span12">
+		<div class="widget">
+			<div class="widget-header">
+				<i class="icon-user"></i>
+				<h3>
+					Lista de usuarios
+				</h3>
+			</div> <!-- /widget-header -->
+			<div class="widget-content">
+				
+				<table class="table table-striped table-bordered">
+					<thead>
+						<tr>
+							<th> Usuario </th>
+							<th> Tipo </th>
+							<th class="td-actions">
+								<button class="btn btn-info pull-right">Buscar</button>
+								<button class="btn btn-success pull-right">Nuevo</button>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td> CDCHIMBOTE </td>
+							<td> CENTRO </td>
+							<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr>
+						<tr>
+							<td> CDHUARAL </td>
+							<td> CENTRO </td>
+							<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr>
+						<tr>
+							<td> KAREN RUÍZ GALLO </td>
+							<td> PROMOTORA </td>
+							<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr>
+						<tr>
+							<td> CDANTA </td>
+							<td> CENTRO </td>
+							<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr>
+						<tr>
+							<td> CDAYACUCHO </td>
+							<td> CENTRO </td>
+							<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr>
+						<tr>
+							<td> admin </td>
+							<td> ADMINISTRADOR </td>
+							<td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr>
 
-	</tbody>
-</table>
-
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
